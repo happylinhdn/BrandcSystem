ed1571eb8fa9c7d82657fb9e4950150cf3caffd6
Linh Do <linh@example.com>
2023-02-01 00:49:52 +0700

    feedback from lana

--- Supplier/admin.py
+++ Supplier/admin.py
@@ -1,11 +1,42 @@
 from django.contrib import admin
-from .models import Supplier
+from .models import Supplier, Fields
+from django.contrib.admin import SimpleListFilter
+from django.utils.translation import gettext as _
 
 # Register your models here.
+class FieldsFilter(SimpleListFilter):
+    title = 'Fields' # or use _('country') for translated title
+    parameter_name = 'field'
+
+    def lookups(self, request, model_admin):
+        all_choices = Fields.choices
+        t_data = [c.fields for c in Supplier.objects.all()]
+        result = []
+        for choice in all_choices:
+            for t in t_data:
+                if choice[0] in t:
+                    result.append(choice)
+                    break
+        return result
+
+    def queryset(self, request, queryset):
+        # to decide how to filter the queryset.
+        # return queryset.filter(fields__contains=self.value())
+        if self.value():
+            return queryset.filter(fields__contains=self.value())
+
+@admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
-    list_display = ['name', 'channel', 'follower', 'kol_tier', 'engagement_rate_absolute_display', 'engagement_rate_percent']
-    list_filter = ['gender', 'channel']
-    search_fields = ['name']
+    list_display = ['name', 'link', 'channel', 'follower', 'kol_tier', 'engagement_rate_percent', 'engagement_rate_absolute_display', 
+    'location', 'year_of_birth', 'gender', 'fields', 'original_cost_picture', 'original_cost_video', 'original_cost_event', 'original_cost_tvc',
+    'kpi', 'discount', 'supplier_name', 'booking_contact_display', 'profile_quotation', 'latest_update', 'handle_by', 'group_chat_name',
+    'group_chat_channel', 'lana_leader' , 'modified_by'
+    ]
+    list_filter = [FieldsFilter, 'kol_tier', 'gender', 'channel', 'location', 'year_of_birth']
+    search_fields = ['name', 'link']
+
+    def kol_tier_2(self, inst):
+        return inst.kol_tier()
 
 # Register your models here.
-admin.site.register(Supplier, SupplierAdmin)
+#admin.site.register(Supplier, SupplierAdmin)
--- Supplier/models.py
+++ Supplier/models.py
@@ -1,66 +1,61 @@
+# -*- coding: utf-8 -*-
 from queue import Empty
 from django.db import models
 from django.utils.translation import gettext as _
 from multiselectfield import MultiSelectField
 from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Fields(models.TextChoices):
     Singer = 'Singer', _('Singer')
-    Dancer = 'Dancer', _('Dancer')
-    Actor_Actress = 'Actor/Actress', _('Actor/Actress')
     Rapper = 'Rapper', _('Rapper')
     DJ = 'DJ', _('DJ')
     Music_Producer = 'Music Producer', _('Music Producer')
-    Director = 'Director', _('Director')
-    Entertaiment = 'Entertaiment', _('Entertaiment')
+    Dancer = 'Dancer', _('Dancer')
+    Streamer = 'Streamer', _('Streamer')
     Content_Creator = 'Content Creator', _('Content Creator')
-    Gymer_Fitness = 'Gymer/Fitness', _('Gymer/Fitness')
-    Teacher_Coach = 'Teacher/ Coach', _('Teacher/ Coach')
-    Model = 'Model', _('Model')
-    Interior_House = 'Interior house', _('Interior house')
+    Reviewer = 'Reviewer', _('Reviewer')
     Footballer = 'Footballer', _('Footballer')
-
-# Create your models here.
-
-# Footballer
-# Beauty/Make up
-# Cosmestic/Skincare
-# Reviewer
-# Fashion
-# Travel
-# Lifestyle
-# Real estate
-# Sport
-# News
-# Education
-# Insurance
-# Hot Mom/Dad
-# Freelancer
-# Streamer
-# Business
-# Office staff
-# Pet
-# Architect
-# Smarthome
-# Houseware
-# Car
-# Accomodation
-# Office staff
-# Game & Esport
-# MC & Editor
-# Life & Social
-# Food & Cooking
-# Technology & Ecommerce
-# Decor & Design
-# Financial & Investment
-# Media & Advertisement
-# Family & Kid
-# Artist & Showbiz
-# Health & Medical
-# Economics & Law
-# Youth & GenZ
-
-
+    Gymer_Fitness = 'Gymer/Fitness', _('Gymer/Fitness')
+    Model = 'Model', _('Model')
+    Showbiz = 'Showbiz', _('Showbiz')
+    Make_up = 'Make-up', _('Make-up')
+    Cosmestic_Skincare = 'Cosmestic/Skincare', _('Cosmestic/Skincare')
+    Fashion = 'Fashion', _('Fashion')
+    Travel = 'Travel', _('Travel')
+    Lifestyle = 'Lifestyle', _('Lifestyle')
+    News = 'News', _('News')
+    Education = 'Education', _('Education')
+    Teacher_Coach = 'Teacher/Coach', _('Teacher/Coach')
+    Office_staff = 'Office staff', _('Office staff')
+    Freelancer = 'Freelancer', _('Freelancer')
+    Business = 'Business', _('Business')
+    Architect = 'Architect', _('Architect')
+    Smarthome = 'Smarthome', _('Smarthome')
+    Home_Appliance = 'Home appliance', _('Home appliance')
+    Interior_House = 'Interior house', _('Interior house')
+    Decor_Design = 'Decor & Design', _('Decor & Design')
+    Investment = 'Investment', _('Investment')
+    Insurance = 'Insurance', _('Insurance')
+    Economics_Law = 'Economics & Law', _('Economics & Law')
+    Capital_Market  = 'Capital Market ', _('Capital Market ')
+    Banking = 'Banking', _('Banking')
+    Kid = 'Kid', _('Kid')
+    Hot_Mom_Dad = 'Hot Mom/Dad', _('Hot Mom/Dad')
+    Automotive = 'Automotive', _('Automotive')
+    Director = 'Director', _('Director')
+    Actor_Actress = 'Actor/Actress', _('Actor/Actress')
+    Health_Medicine = 'Health & Medicine', _('Health & Medicine')
+    Youth_GenZ = 'Youth & GenZ', _('Youth & GenZ')
+    Media_Advertisement = 'Media & Advertisement', _('Media & Advertisement')
+    Game_Esport = 'Game & Esport', _('Game & Esport')
+    MC_Editor = 'MC & Editor', _('MC & Editor')
+    Food_Drink = 'Food & Drink', _('Food & Drink')
+    Technology_Ecommerce = 'Technology & Ecommerce', _('Technology & Ecommerce')
+    Celeb = 'Celeb', _('Celeb')
+    General = 'General', _('General')
+    Other = 'Other', _('Other')
+    
 class SupplierChannel(models.TextChoices):
     FB_COMMUNITY = 'Facebook Community', _('Facebook Community')
     FB_PERSONAL = 'Facebook Personal', _('Facebook Personal')
@@ -93,18 +88,31 @@ class Supplier(models.Model):
     link = models.CharField(max_length=300)
     channel = models.CharField(max_length=18, choices=SupplierChannel.choices, )
     follower = models.CharField(max_length=20) # 17k -> 17000, 17M -> 17000000 
-    engagement_rate_percent = models.CharField(max_length=10)
+    follower_2 = models.FloatField(editable=False, null=True, blank=True)
+    kol_tier = models.CharField(max_length=20, editable=False, null=True, blank=True)
+
+    engagement_rate_percent = models.FloatField(verbose_name='ER(%)', max_length=10)
+    engagement_rate_absolute = models.FloatField(verbose_name='ER (Ab.)', max_length=20, editable=False, null=True, blank=True)
+    engagement_rate_absolute_display = models.CharField(verbose_name='ER (Ab.)', max_length=20, editable=False, null=True, blank=True)
+
     location = models.CharField(max_length=3, choices=Location.choices, default=Location.HCM, )
-    year_of_birth = models.CharField(max_length=4)
+    year_of_birth = models.IntegerField(
+        default=1900,
+        validators=[
+            MaxValueValidator(2030),
+            MinValueValidator(1900)
+        ], blank=True)
     gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.Male, )
     fields = MultiSelectField(choices=Fields.choices, max_choices=10, max_length=500)
     #ORIGINAL COST
-    original_cost_picture = models.CharField(max_length=50, null=True, blank=True)
-    original_cost_video = models.CharField(max_length=50, null=True, blank=True)
-    original_cost_event = models.CharField(max_length=50, null=True, blank=True)
-    kpi = models.CharField(max_length=50, null=True, blank=True)
+    original_cost_picture = models.FloatField(verbose_name='ORIGINAL COST - PICTURE', null=True, blank=True)
+    original_cost_video = models.FloatField(verbose_name='ORIGINAL COST - VIDEO', null=True, blank=True)
+    original_cost_event = models.FloatField(verbose_name='ORIGINAL COST - EVENT',  null=True, blank=True)
+    original_cost_tvc = models.FloatField(verbose_name='ORIGINAL COST - TVC', null=True, blank=True)
+    kpi = models.FloatField(verbose_name='KPI',max_length=50, null=True, blank=True)
+
     #DISCOUNT, SUPPLIER NAME
-    discount = models.CharField(max_length=50, null=True, blank=True)
+    discount = models.FloatField(max_length=50, null=True, blank=True)
     supplier_name = models.CharField(max_length=100, null=True, blank=True)
 
     #BOOKING CONTACT: Name, Phone, Email
@@ -112,14 +120,14 @@ class Supplier(models.Model):
     booking_contact_phone = models.CharField(max_length=15, null=True, blank=True)
     booking_contact_email = models.CharField(max_length=50, null=True, blank=True)
 
-    #LATEST UPDATE
+    profile_quotation = models.CharField(verbose_name='PROFILE/QUOTATION', max_length=50, null=True, blank=True)
     latest_update = models.DateTimeField()
     #HANDLE BY
     handle_by = models.CharField(max_length=100, blank=True)
     #GROUP CHAT NAME
     group_chat_name = models.CharField(max_length=100, blank=True)
-    #Kênh (Zalo, Viber or Facebook)
-    kenh = models.CharField(max_length=2, choices=Kenh.choices, default=Kenh.Zalo, )
+    #Kênh (Zalo, Viber or Facebook) 
+    group_chat_channel = models.CharField(verbose_name='Group Chat Channel', max_length=2, choices=Kenh.choices, default=Kenh.Zalo, )
     #Set Leader Mode to Ms. Lana
     lana_leader = models.BooleanField(default=False)
     #History
@@ -150,8 +158,7 @@ class Supplier(models.Model):
                 print("Can not convert follower")
         return value
 
-    @property
-    def kol_tier(self):
+    def kol_tier_detect(self):
         if self.follower_value() in range(1, 10000):
             return "Nano influencer"
         elif self.follower_value() in range(10000, 50000):
@@ -165,20 +172,19 @@ class Supplier(models.Model):
         else:
             return "Unknown"
 
-    def engagement_rate_absolute(self):
+    def engagement_rate_absolute_calc(self):
         follower_value = self.follower_value()
         rate = 0.0
         try:
-            rate = float(self.engagement_rate_percent.replace(",","."))
+            rate = self.engagement_rate_percent
         except:
             rate = 100
             print("Can not convert engagement_rate_percent")
 
         return follower_value * rate/100
 
-    @property
-    def engagement_rate_absolute_display(self):
-        engagement_rate_absolute = self.engagement_rate_absolute()
+    def engagement_rate_absolute_display_calc(self):
+        engagement_rate_absolute = self.engagement_rate_absolute_calc()
         if engagement_rate_absolute >= 1000000:
             temp = engagement_rate_absolute/1000000
             return "{0}M".format(round(temp, 2))
@@ -188,9 +194,32 @@ class Supplier(models.Model):
             return "{0}K".format(round(temp, 2))
         
         return "{0}".format(round(engagement_rate_absolute, 2))
-    
-    engagement_rate_absolute_display.fget.short_description = 'Engagement rate absolute'
 
+    @property
+    def booking_contact_display(self):
+        return """Name:{0} - Phone: {1} - Email:{2}""".format(self.booking_contact_name, self.booking_contact_phone, self.booking_contact_email)
+
+    booking_contact_display.fget.short_description = 'Booking contact'
+
+    def save(self, *args, **kwargs):
+        self.follower_2 = self.follower_value()
+        self.kol_tier = self.kol_tier_detect()
+        self.engagement_rate_absolute = self.engagement_rate_absolute_calc()
+        self.engagement_rate_absolute_display = self.engagement_rate_absolute_display_calc() 
+
+        all_choices = Fields.choices
+        t_data = [c.fields for c in Supplier.objects.all()]
+        result = ()
+        for choice in all_choices:
+            print(choice[1])
+            # for t in t_data:
+            #     if choice.value() in t:
+            #         result.append(choice)
+            #         break
+
+        super(Supplier, self).save(*args, **kwargs)
+
+        
     def parse_to_json(self):
         return {
             'name': self.name,
