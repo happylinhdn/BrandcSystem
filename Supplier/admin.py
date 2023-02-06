29f5848b92083b8b6e709b522d1fddf0cd493f38
Linh Do <happy.linhdn@gmail.com>
2023-02-06 17:21:33 +0700

    index id

--- Supplier/admin.py
+++ Supplier/admin.py
@@ -21,6 +21,9 @@ class SupplierAdmin(ImportMixin, admin.ModelAdmin, ExportCsvMixin):
     indexCnt = 0
 
     def index_counter(self, obj):
+        print("index_counter num_pages")
+        # print(self.paginator.count)
+        # print(self.paginator.num_pages)
         count = Supplier.objects.all().count()
         if self.indexCnt < count:
             self.indexCnt += 1
@@ -46,7 +49,7 @@ class SupplierAdmin(ImportMixin, admin.ModelAdmin, ExportCsvMixin):
         }),
     )
 
-    list_display = ['index_counter', 'name', 'channel_display', 'follower', 'kol_tier', 'engagement_rate_percent', 'engagement_rate_absolute_display', 
+    list_display = ['id', 'name', 'channel_display', 'follower', 'kol_tier', 'engagement_rate_percent', 'engagement_rate_absolute_display', 
     'location', 'year_display', 'gender', 'fields', 'original_cost_picture', 'original_cost_video', 'original_cost_event', 'original_cost_tvc',
     'kpi', 'discount', 'supplier_name', 'booking_contact', 'profile_display', 'latest_update', 'handle_by', 'group_chat_name',
     'group_chat_channel', 'lana_leader' , 'modified_by'
@@ -54,7 +57,7 @@ class SupplierAdmin(ImportMixin, admin.ModelAdmin, ExportCsvMixin):
     list_display_links  = ['name',]
     list_filter = [CostRangeFilter, 'kol_tier', 'gender', 'year_of_birth', 'channel', FieldsFilter, 'location', ]
     search_fields = ['name', 'link']
-    list_per_page = 25
+    list_per_page = 200
     actions = ["export_as_xls"]
     ordering = ['id']
 
