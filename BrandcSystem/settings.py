5e753dd69501424772066db845b2b3cdfe2081fd
root <root@instance-1.asia-east1-b.c.brandcvm.internal>
2023-03-04 09:16:06 +0000

    migrate to postgres

--- BrandcSystem/settings.py
+++ BrandcSystem/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'BrandcSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'brandc',
+        'USER': 'brandcuser',
+        'PASSWORD':'Aesx5099',
+        'HOST':'localhost',
+        'PORT':'5432'
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE':'django.db.backends.postgresql_psycopg2',
-#         'NAME':'brandc',
-#         'USER': 'linh',
-#         'PASSWORD':'Aesx5099',
-#         'HOST':'localhost',
-#         'PORT':'5432'
-#     }
-# }
 
 
 # Password validation
